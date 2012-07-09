d050e1ffb88df30462658e7535e30dbf6b73b3b1
John Smart <smartj@gmail.com>
2012-07-09 00:43:35 -0700

    Added set, first, and all functions

--- src/dotty/Dotty.php
+++ src/dotty/Dotty.php
@@ -52,7 +52,6 @@ class Dotty {
 		$dataCursor		=& $data;
 		$instructions	= self::parseNotation($notation);
 
-
 		$pathSoFar	= array();
 		foreach ($instructions as $x) {
 			if (is_int($x)) {
@@ -73,5 +72,117 @@ class Dotty {
 		}
 
 		return $dataCursor;
+	}
+
+	/**
+	 * Query an array for a set of data. The last symbol in the notation designates a set
+	 *
+	 * @static
+	 * @param string $notation		Dot notation
+	 * @param array& $dataCursor	Data to search through
+	 * @return array&
+	 */
+	public static function set($notation, array &$data) {
+		$instructions	= self::parseNotation($notation);
+
+		// the last symbol is the
+		$setKey			= array_pop($instructions);
+
+		// get the sub tree
+		if (empty($instructions)) {
+			return $data[$setKey];
+		}
+
+		try {
+			$subData		= &self::dot(implode('.', $instructions), $data);
+		} catch (\InvalidArgumentException $iae) {
+			return array();
+		}
+
+		return self::all($setKey, $subData);
+	}
+
+	/**
+	 * Look through an array of arrays for the first key that matches $ket
+	 * @static
+	 * @param string $key		The key to match
+	 * @param array& $data		The data to search through
+	 * @return mixed&
+	 */
+	public static function &first($key, array &$data) {
+		$result = array();
+		self::r_first($key, $data, $result);
+
+		if (empty($result)) {
+			throw new \InvalidArgumentException(sprintf('"%s" does not exist', $key));
+		}
+
+		return $result[0];
+	}
+
+	/**
+	 * Recursive helper
+	 * @static
+	 * @param $key
+	 * @param $data
+	 * @param array $result
+	 */
+	private static function r_first($key, &$data, array &$result) {
+		if (!is_array($data)) {
+			// base-case
+			return;
+		}
+
+		foreach ($data as $subKey => &$dataCursor) {
+			if ($subKey === $key) {
+				// base-case
+				$result[] =& $dataCursor;
+				return;
+			}
+
+			// induction step
+			self::r_first($key, $dataCursor, $result);
+			if (!empty($result)) {
+				// base-case
+				return;
+			}
+		} unset($dataCursor);
+	}
+
+	/**
+	 * Look through an array of arrays for all key-matches of $key
+	 * @static
+	 * @param string $key		The key to match
+	 * @param array& $data		The data to search through
+	 * @return array&
+	 */
+	public static function &all($key, array &$data) {
+		$results = array();
+		self::r_all($key, $data, $results);
+		return $results;
+	}
+
+	/**
+	 * Recursive helper
+	 * @static
+	 * @param $key
+	 * @param $data
+	 * @param array $results
+	 */
+	private static function r_all($key, &$data, array &$results) {
+		if (!is_array($data)) {
+			// base-case
+			return;
+		}
+
+		foreach ($data as $subKey => &$dataCursor) {
+			if ($subKey === $key) {
+				// base-case
+				$results[] =& $dataCursor;
+			}
+
+			// induction step
+			self::r_all($key, $dataCursor, $results);
+		} unset($dataCursor);
 	}
 }
--- tests/DottyTest.php
+++ tests/DottyTest.php
@@ -9,7 +9,7 @@ use dotty\Dotty;
 
 class DottyTest extends \PHPUnit_Framework_TestCase
 {
-	public function test0() {
+	public function testDot() {
 		$data = array('name' => 'Scrooge McDuck');
 		$found = &Dotty::dot('name', $data);
 		$this->assertEquals('Scrooge McDuck', $found);
@@ -17,9 +17,7 @@ class DottyTest extends \PHPUnit_Framework_TestCase
 		$data = array('Daffy Duck');
 		$found = &Dotty::dot('[0]', $data);
 		$this->assertEquals('Daffy Duck', $found);
-	}
 
-	public function test1() {
 		$data = array(
 			'piece' => array(
 				'name' => 'Mozarts 9th',
@@ -52,13 +50,116 @@ class DottyTest extends \PHPUnit_Framework_TestCase
 		$found = &Dotty::dot('piece.measures[1].notes[2]', $data);
 		$this->assertEquals($data['piece']['measures'][1]['notes'][2], $found);
 
+		// test mutation
 		$found = &Dotty::dot('piece.measures[1].notes[2]', $data);
 		$found	= 14;
 		$this->assertEquals($found, $data['piece']['measures'][1]['notes'][2]);
-	}
 
-	public function testEx() {
 		$this->setExpectedException('\InvalidArgumentException');
 		Dotty::dot('foobar', $data = array());
+	}
+
+	public function testFirst() {
+		$data = array(
+			array(
+				'notes' => array(1, 2, 3),
+				'intensity' => 10
+			),
+			array(
+				'notes' => array(1, 4, 5),
+				'intensity' => 7
+			),
+			array(
+				'notes' => array(2, 3, 4, 5),
+				'intensity' => 5
+			)
+		);
+		$forte	= &Dotty::first('intensity', $data);
+		$this->assertEquals(10, $forte);
+
+		// test mutation
+		$forte	= 11;
+		$this->assertEquals(11, $data[0]['intensity']);
+
+		$this->setExpectedException('\InvalidArgumentException');
+		Dotty::first('foobar', $data);
+	}
+
+	public function testAll() {
+		$data = array(
+			array(
+				'notes' => array(1, 2, 3),
+				'intensity' => 10
+			),
+			array(
+				'notes' => array(1, 4, 5),
+				'intensity' => 7
+			),
+			array(
+				'notes' => array(2, 3, 4, 5),
+				'intensity' => 5
+			)
+		);
+
+		$set	= &Dotty::all('intensity', $data);
+		$this->assertEquals(array(10, 7, 5), $set);
+
+		// test mutation
+		$set[1]	= 10;
+		$this->assertEquals(10, $data[1]['intensity']);
+	}
+
+	public function testSet() {
+		$data = <<<DATA
+{
+   "data": [
+      {
+         "name": "Farenheit 451",
+         "category": "Book",
+         "id": "114836791862134",
+         "created_time": "2010-09-30T19:36:29+0000"
+      },
+      {
+         "name": "Coders at work",
+         "category": "Book",
+         "id": "104178566283955",
+         "created_time": "2010-09-30T19:20:50+0000"
+      },
+      {
+         "name": "Age of Spiritual Machines",
+         "category": "Book",
+         "id": "114114031939540",
+         "created_time": "2010-09-30T19:20:50+0000"
+      },
+      {
+         "name": "The Internets",
+         "category": "Book",
+         "id": "104303812934779",
+         "created_time": "2010-05-03T19:36:18+0000"
+      },
+      {
+         "name": "Ender's Game",
+         "category": "Book",
+         "id": "108178452537550",
+         "created_time": "2010-05-03T19:36:18+0000"
+      },
+      {
+         "name": "Catcher In The Rye",
+         "category": "Book",
+         "id": "115946728419025",
+         "created_time": "2010-05-03T19:36:18+0000"
+      },
+      {
+         "name": "George Orwell",
+         "category": "Author",
+         "id": "108663695825202",
+         "created_time": "2010-05-03T19:36:18+0000"
+      }
+   ]
+}
+DATA;
+		$data	= json_decode($data, true);
+		$names	= Dotty::set('data.name', $data);
+		$this->assertEquals(array("Farenheit 451","Coders at work","Age of Spiritual Machines","The Internets","Ender's Game","Catcher In The Rye","George Orwell"), $names);
 	}
 }
